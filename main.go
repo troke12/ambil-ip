9d147d9b0914176dc862c95356deb0864682bdaa
troke12 <jame_glove@yahoo.com>
2021-02-10 10:45:30 +0000

    Tanpa menggunakan JSON

--- main.go
+++ main.go
@@ -6,30 +6,11 @@ import (
 	"net/http"
 	"io/ioutil"
 	"strings"
-	"encoding/json"
 )
-
-type AmbilIP struct {
-	IP            string `json:"ip"`
-	City          string `json:"city"`
-	Region        string `json:"region"`
-	Country       string `json:"country"`
-	CountryFull   string `json:"country_full"`
-	Continent     string `json:"continent"`
-	ContinentFull string `json:"continent_full"`
-	Loc           string `json:"loc"`
-	Postal        string `json:"postal"`
-}
-
-func perror(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
 func get_ip() {
 	kustom := "101.0.4.6"
-	url := "http://ip.zxq.co/" + kustom
+	url := "http://ip.zxq.co/" + kustom + "/country"
 
 	res, err := http.Get(url)
 
@@ -43,9 +24,7 @@ func get_ip() {
 		panic(err.Error())
 	}
 
-	var data AmbilIP
-	json.Unmarshal(body, &data)
-	if strings.Contains(data.Country, "ID"){
+	if strings.Contains(string(body), "ID"){
 		fmt.Printf("\nBenar\n")
 	} else {
 		fmt.Printf("\nSalah\n")
